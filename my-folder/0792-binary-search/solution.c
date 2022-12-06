845c7912cf59749b17b14dd7719a11066c3e7291
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-06 07:15:50 +0000

    [LeetCode Sync] Runtime - 42 ms (5.14%), Memory - 7.1 MB (12.41%)

--- my-folder/0792-binary-search/solution.c
+++ my-folder/0792-binary-search/solution.c
@@ -1,18 +1,13 @@
 int search(int* nums, int numsSize, int target){
         int start = 0, end = numsSize-1;
-        if (nums[start] == target)
-                return start;
-        if (nums[end] == target)
-                return end;
-        char dir = nums[start] < nums[end] ? 1 : -1;
-        while ((end-start) > 1){
-                int middle = (start + end) / 2;
-                if (nums[middle] == target)
-                        return middle;
-                if (dir*(target - nums[middle]) > 0)
-                        start = middle;
-                else
-                        end = middle;
+        while (start < end){
+            int middle = (start + end) / 2;
+            if (nums[middle] == target)
+                return middle;
+            if (target > nums[middle])
+                start = middle + 1;
+            else
+                end = middle - 1;
         }
-        return -1;
+        return nums[start] == target ? start : -1;
 }
