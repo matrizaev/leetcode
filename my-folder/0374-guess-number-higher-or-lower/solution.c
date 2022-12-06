3de1afaa2ec97ebdc67a311905a41258dc1e6830
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-06 07:48:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.4 MB (49.47%)

---
+++ my-folder/0374-guess-number-higher-or-lower/solution.c
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+int guessNumber(int n){
+    unsigned int start = 0, end = n;
+    while (start < end){
+        unsigned int middle = (start + end) >> 1;
+        int trial = guess(middle);
+        switch(trial){
+            case 0:
+                return middle;
+            case -1:
+                end = middle - 1;
+                break;
+            default:
+                start = middle + 1;
+        }
+        // if (trial < 0)
+        //     end = middle - 1;
+        // else if (trial > 0)
+        //     start = middle + 1;
+        // else
+        //     return middle;         
+    }
+    return start;	
+}
