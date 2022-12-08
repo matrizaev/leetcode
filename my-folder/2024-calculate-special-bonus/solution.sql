781e968f8cc4ab472c7780e8397f0abd05c1af60
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-08 12:16:12 +0000

    [LeetCode Sync] Runtime - 776 ms (37.51%), Memory - 0B (100.00%)

---
+++ my-folder/2024-calculate-special-bonus/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select employee_id, 
+CASE
+WHEN MOD(employee_id, 2) = 1  AND name NOT LIKE 'M%'
+THEN salary
+ELSE 0 
+END as bonus
+FROM employees order by employee_id;
