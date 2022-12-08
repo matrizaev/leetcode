97b47d7697f29aa70b43e10db1fdefdccbe0d47e
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-08 12:55:13 +0000

    [LeetCode Sync] Runtime - 489 ms (43.88%), Memory - 0B (100.00%)

---
+++ my-folder/1625-group-sold-products-by-the-date/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select sell_date, count(distinct product) num_sold,
+group_concat(DISTINCT product ORDER BY product ASC SEPARATOR ',') products
+from activities
+group by sell_date
+order by sell_date
