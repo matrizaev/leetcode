7677fd4fca1aebca642adf936fe567e9aa56240d
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-09 08:33:31 +0000

    [LeetCode Sync] Runtime - 2168 ms (26.04%), Memory - 0B (100.00%)

---
+++ my-folder/1268-market-analysis-i/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with data as
+(select buyer_id, count(*) orders_in_2019 from orders
+where year(order_date) = 2019
+group by buyer_id)
+select user_id buyer_id, join_date, coalesce(orders_in_2019, 0) orders_in_2019
+from data
+right join users on users.user_id = data.buyer_id
