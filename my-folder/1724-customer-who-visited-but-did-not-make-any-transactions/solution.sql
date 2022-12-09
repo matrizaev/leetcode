ed3f880952231fe4dc516475194524870b4f7562
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-09 09:28:07 +0000

    [LeetCode Sync] Runtime - 1432 ms (43.93%), Memory - 0B (100.00%)

---
+++ my-folder/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select customer_id,
+sum(case when transaction_id is NULL then 1 else 0 end) count_no_trans
+from visits left join transactions on visits.visit_id = transactions.visit_id
+group by customer_id
+having count_no_trans > 0
