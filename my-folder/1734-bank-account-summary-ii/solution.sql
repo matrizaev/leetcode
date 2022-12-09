8ca4ee7be8d8b4e0b88b7db20ed95c79a278ca5e
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-09 09:14:14 +0000

    [LeetCode Sync] Runtime - 1226 ms (32.32%), Memory - 0B (100.00%)

---
+++ my-folder/1734-bank-account-summary-ii/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select name, sum(amount) balance
+from users inner join transactions on users.account = transactions.account
+group by name
+having sum(amount) > 10000
