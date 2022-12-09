ae07d880947e00cdb9e1c783b68ae9877d9baea1
Viacheslav Matrizaev <matrizaev@gmail.com>
2022-12-09 11:09:29 +0000

    [LeetCode Sync] Runtime - 984 ms (99.98%), Memory - 0B (100.00%)

---
+++ my-folder/0607-sales-person/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+with sales as (
+select sales_id, order_id from orders inner join company using(com_id) where name='RED'
+)
+select p.name
+from salesperson p
+left join sales s USING(sales_id)
+group by p.name
+having count(order_id) = 0
+
+
