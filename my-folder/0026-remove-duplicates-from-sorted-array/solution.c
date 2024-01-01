cf34b16a23b5083cc5e07f4c531f2ab8f63c17d6
Viacheslav Matrizaev <matrizaev@gmail.com>
2024-01-01 13:17:12 +0000

    [LeetCode Sync] Runtime - 26 ms (8.36%), Memory - 8 MB (7.71%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.c
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.c
@@ -1,12 +1,8 @@
-int removeDuplicates(int* nums, int numsSize){
-    if (numsSize == 0 || nums == NULL)
-	    return 0;
-    int k = 0;
-	for (ssize_t i = 1; i < numsSize; i++){
-		if (nums[i] != nums[k]){
-			k++;
-			nums[k] = nums[i];
-		}
-	}
-	return k+1;
+int removeDuplicates(int* nums, int numsSize) {
+    size_t j = 1;
+    for (size_t i = 1; i < numsSize; i++){
+        if (nums[i] > nums[i-1])
+            nums[j++] = nums[i];
+    }
+    return j;
 }
