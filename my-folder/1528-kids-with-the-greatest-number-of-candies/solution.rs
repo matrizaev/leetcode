99a229a64f466b9958aa8db4804bd60af67aa29d
Viacheslav Matrizaev <matrizaev@gmail.com>
2024-11-07 19:49:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.1 MB (45.41%)

---
+++ my-folder/1528-kids-with-the-greatest-number-of-candies/solution.rs
@@ -0,0 +1,7 @@
+impl Solution {
+    pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
+        let max = candies.iter().max().unwrap();
+
+        candies.iter().map(|c| c + extra_candies >= *max).collect()
+    }
+}
